b6b796938f72a3444fb2340beafe33704ec823ac
Manidhar Reddy <manidhar298@gmail.com>
2023-10-17 08:27:27 +0530

    add Inheritance.java file

---
+++ Day1/Task2/Inheritance.java
@@ -0,0 +1,24 @@
+package Day1.Task2;
+class Animal {
+    String walk(){
+        return "I am walking";
+    }
+}
+class Bird extends Animal {
+    String fly(){
+        return "I am flying";
+    }
+}
+class SingingBirds extends Bird{
+    String sing(){
+        return "I am singing";
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        SingingBirds parrot = new SingingBirds();
+        System.out.println(parrot.walk());
+        System.out.println(parrot.fly());
+        System.out.println(parrot.sing());
+    }
+}
