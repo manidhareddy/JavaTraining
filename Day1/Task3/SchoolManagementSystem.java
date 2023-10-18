5bdf192f7fc6807f4123e24b1b066bbbde2c6ac5
Manidhar Reddy <manidhar298@gmail.com>
2023-10-18 16:03:25 +0530

    added child class attributes & also implemented encapsulation

--- Day1/Task3/SchoolManagementSystem.java
+++ Day1/Task3/SchoolManagementSystem.java
@@ -3,67 +3,83 @@ class Staff{
     //base class where all common attributes and methods are declared in this class
     //staff class is inherited by Teacher,Administration and SupportStaff class to acquire common features
     //hierarchical level inheritance
-    String name;
-    int employeeID;
-    int salary;
-    String role;
-    String responsibilities;
+    private String name;
+    private int employeeID;
+    private int salary;
+
     public Staff(String name,int employeeID,int salary){
         this.name = name;//this represents current object
         this.employeeID = employeeID;
         this.salary = salary;
     }
-    public void staffDetails(){
-        System.out.println("Staff Details:-");
-        System.out.println("staff name: "+this.name + "\nemployeeID: "+ this.employeeID + " \nRole: "+this.role +" \nResponsibilities: "+this.responsibilities);
+    public int getEmployeeID() {
+        return employeeID;
+    }
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 }
 class Teacher extends Staff{
     //inherit staff class to use common attributes
-    String subject;
+    private String subject;
     public Teacher(String name, int employeeID, int salary,String subject) {
         super(name, employeeID, salary);
-        this.role = "Teaching";
-        this.responsibilities = "To teach students and help them grow their academic knowledge.";
         this.subject = subject;
     }
-    public void teach(){
-        System.out.println(this.name + " teaches "+this.subject);
+    public String getSubject() {
+        return subject;
+    }
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 }
 class Administrators extends Staff{
     //inherit staff class
-    String department;
-    public Administrators(String name,int employeeID,int salary,String department,String role,String responsibilities){
+    private String department;
+    public Administrators(String name,int employeeID,int salary,String department){
         super(name,employeeID,salary);//call base class constructor
         this.department = department;
-        this.role = role;
-        this.responsibilities = responsibilities;
     }
-    public void manage(){
-        System.out.println(this.name + " manages "+this.department + " department");
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
     }
 }
 class SupportStaff extends Staff{
-    public SupportStaff(String name, int employeeID, int salary) {
+    private String role;
+    public SupportStaff(String name, int employeeID, int salary,String role) {
         super(name, employeeID, salary);//call base class constructor
-        this.role = "provide support";
-        this.responsibilities = "clear doubt for students";
+        this.role = role;
     }
-    public void teacherOnLeave(){
-        System.out.println("carry out specific tasks set by the main teacher");
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
     }
 }
 public class SchoolManagementSystem {
     public static void main(String[] args) {
-        Teacher teacher1 = new Teacher("Ravi",823,23_000,"Math");
-        Administrators admin = new Administrators("Rajesh",923,20_000,"Admissions","Collect details","To take new admissions");
-        SupportStaff supportStaff = new SupportStaff("Kiran",780,15_000);
-        teacher1.staffDetails();
-        teacher1.teach();
-        admin.staffDetails();
-        admin.manage();
-        supportStaff.staffDetails();
-        supportStaff.teacherOnLeave();
+        Teacher teacher = new Teacher("John Doe", 101, 50000, "Mathematics");
+        Administrators admin = new Administrators("Rajesh",923,20_000,"Admissions");
+        SupportStaff supportStaff = new SupportStaff("Kiran",780,15_000,"Custodian");
+        System.out.println("Teacher Details: " + teacher.getName() + ", " + teacher.getEmployeeID() + ", " + teacher.getSalary() + ", " + teacher.getSubject());
+        System.out.println("Admin Details: "+admin.getName()+", "+admin.getEmployeeID()+", "+admin.getSalary()+", "+admin.getDepartment());
+        System.out.println("Support staff: "+supportStaff.getName()+", "+supportStaff.getEmployeeID()+", "+supportStaff.getSalary());
     }
 }
