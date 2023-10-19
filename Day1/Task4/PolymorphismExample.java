eda69bbd78035170fcf2b33743849e6784e9a00e
Manidhar Reddy <manidhar298@gmail.com>
2023-10-19 15:21:35 +0530

    used overriding concept

--- Day1/Task4/PolymorphismExample.java
+++ Day1/Task4/PolymorphismExample.java
@@ -1,56 +1,61 @@
 package Day1.Task4;
-class Calculator{
-    public Calculator(){}
-    //method overloading is a compile time polymorphism
-    //When two or more methods with same name but different parameters
-    public int addition(int number1,int number2){
-        return number1 + number2;
-    }
-    public int addition(int number1,int number2,int number3){
-        return number1 + number2 + number3;
-    }
-    public double addition(double number1 ,double number2){
-        return number1 + number2;
-    }
-    public double addition (double number1, double number2 , double number3){
-        System.out.println("addition with three parameters is called");
-        return number1 + number2 + number3;
-    }
-    public double addition(double ...numbers){
-        //it is a vararg method it takes variable length arguments
-        //it basically takes an array of values
-
-        double value = 0;
-        for(double number : numbers){
-            value += number;
-        }
-        return value;
-    }
-    public double subtraction(double number1 , double number2){
-        return number1 - number2;
-    }
-    public double subtraction(double number1,double number2,double number3){
-        return number1 - number2 - number3;
-    }
-    public int multiplication(int number1,int number2){
-        return number1 * number2;
-    }
-    public long multiplication(int number1,int number2,int number3){
-        return (long) number1 * number2 * number3;
-    }
-
-    public int division(int number1,int number2){
-        return number1 / number2;
-    }
-    public int division(int number1,int number2,int number3){
-        return number1/number2/number3;
+class Calculator {
+    public void operation(double... numbers){
+        System.out.println("perform some calculation operation");
     }
 }
+class Addition extends Calculator{
+    @Override
+    public void operation(double... numbers){
+        double sum =0;
+        if(numbers.length<=2){ System.out.println("Give more than two numbers"); return;}
+        for(double number : numbers){
+            sum += number;
+        }
+        System.out.println("Addition of Numbers :"+sum);
+    }
+}
+class Multiplication extends Calculator{
+    @Override
+    public void operation(double... numbers){
+        double value =0;
+        if(numbers.length<=2){ System.out.println("Give more than two numbers"); return;}
+        for(double number : numbers){
+            value *= number;
+        }
+        System.out.println("Multiplication of Numbers :"+value);
+    }
+}
+class Subtraction extends Calculator{
+    @Override
+    public void operation(double... numbers){
+        double value =0;
+        if(numbers.length<=2){ System.out.println("Give more than two numbers"); return;}
+        for(double number : numbers){
+            value -= number;
+        }
+        System.out.println("Subtraction of Numbers :"+value);
+    }
+}
+class Division extends Calculator{
+    @Override
+    public void operation(double... numbers){
+        double value =0;
+        if(numbers.length<=2){ System.out.println("Give more than two numbers"); return;}
+        for(double number : numbers){
+            value /= number;
+        }
+        System.out.println("Division of Numbers :"+value);
+    }
+}
+
 public class PolymorphismExample {
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println("Addition of two numbers: "+cal.addition(23,45));
-        System.out.println("Addition of two numbers: "+cal.addition(256,78,12));
-        System.out.println("Multiplication of three numbers: "+cal.multiplication(23,4,5));
+        Calculator addition = new Addition();
+        addition.operation(2,3,4,5);
+        Calculator multiplication = new Multiplication();
+        multiplication.operation(2,3,4);
+        Calculator subtraction = new Subtraction();
+        subtraction.operation(34,2,6,7,8);
     }
 }
