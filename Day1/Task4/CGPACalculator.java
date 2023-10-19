4cdd63c1ff34f0a8a496a13c5ac4f82f91b220f1
Manidhar Reddy <manidhar298@gmail.com>
2023-10-19 14:59:11 +0530

    input from cmd using loop

--- Day1/Task4/CGPACalculator.java
+++ Day1/Task4/CGPACalculator.java
@@ -1,7 +1,10 @@
 package Day1.Task4;
+
+import java.util.Scanner;
 class Subject {
     private String subjectName;
     private double marks;
+    public Subject(){}
     public Subject(String name,double marks){
         this.subjectName = name;
         this.marks = marks;
@@ -18,25 +21,23 @@ class Subject {
     public void setMarks(double marks) {
         this.marks = marks;
     }
-    public double getGradePoint(){
-        return this.marks/10;
+    public static double getCGPA(Subject[] subjects) {
+        double sumOfGradeMarks = 0;
+        for (Subject subject : subjects) {
+            sumOfGradeMarks += subject.getMarks() / 10;
+        }
+        return sumOfGradeMarks / 6;
     }
 }
 public class CGPACalculator {
     public static void main(String[] args) {
         Subject[] subjects = new Subject[6];
-        subjects[0] = new Subject("DataStructures",90);
-        subjects[1] = new Subject("Python",80);
-        subjects[2] = new Subject("DBMA",60);
-        subjects[3] = new Subject("Java",75);
-        subjects[4] = new Subject("Computer Networks",63);
-        subjects[5] = new Subject("Machine Learning",67);
-        double cgpa;
-        double sumOfGradePoints = 0;
-        for(Subject subject : subjects){
-            sumOfGradePoints += subject.getGradePoint();
+        Scanner sc = new Scanner(System.in);
+        for(int i =0;i<6;i++){
+            subjects[i] = new Subject();
+            subjects[i].setSubjectName(sc.next());
+            subjects[i].setMarks(sc.nextDouble());
         }
-        System.out.println(sumOfGradePoints / 6);
-
+        System.out.println("CGPA: "+Subject.getCGPA(subjects));
     }
 }
