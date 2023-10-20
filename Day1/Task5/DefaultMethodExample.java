7b1a43dfd18c843662521f702ab1d10f64a5ee8e
Manidhar Reddy <manidhar298@gmail.com>
2023-10-20 13:34:19 +0530

    spell mistake

--- Day1/Task5/DefaultMethodExample.java
+++ Day1/Task5/DefaultMethodExample.java
@@ -1,7 +1,7 @@
 package Day1.Task5;
 interface TV {
     public void display();
-    default void dispaly3D(){
+    default void display3D(){
         System.out.println("Display 3D pictures");
     }
 }
@@ -11,10 +11,23 @@ class SmartTV implements TV{
         System.out.println("Display moving images");
     }
 }
+class AndroidTV implements TV{
+    @Override
+    public void display(){
+        System.out.println("display images");
+    }
+    @Override
+    public void display3D(){
+        System.out.println("display 3d moving images");
+    }
+
+}
 public class DefaultMethodExample {
     public static void main(String[] args) {
         TV tv = new SmartTV();
         tv.display();
-        tv.dispaly3D();
+        tv.display3D();
+        TV tv1 = new AndroidTV();
+        tv1.display3D();
     }
 }
