ef63ba3339dae4fa5e50b3941e06ab5dbcb211f3
Manidhar Reddy <manidhar298@gmail.com>
2023-10-20 10:40:27 +0530

    small changes

--- Day1/Task5/ConstructorChaining.java
+++ Day1/Task5/ConstructorChaining.java
@@ -1,22 +1,21 @@
 package Day1.Task5;
 class Constructor{
-    //constructor chaining using constructor overloading
-    //constructor are special methods they don't contain return type
     public Constructor(){
+        this("mani");
         System.out.println("It is a non-Parametrized constructor");
+        //calling a constructor from another constructor
     }
     public Constructor(String name){
-        this();//call current  object constructor
+        this(name,22);
         System.out.println("Constructor with one parameter " + name );
     }
     public Constructor(String name,int age){
-        this(name);
         System.out.println("Constructor with two parameters "+ name +" ," +age);
     }
 }
 public class ConstructorChaining {
     public static void main(String[] args) {
-        Constructor const1 = new Constructor("manidhar",22);
+        Constructor const1 = new Constructor();
     }
 
 }
