578130adb73944c62af2f1724e48b4d4ed9a7382
Manidhar Reddy <manidhar298@gmail.com>
2023-10-20 16:36:25 +0530

    OnlineShoppingSystem.java file added

---
+++ Day1/EvaluationTask1/OnlineShoppingSystem.java
@@ -0,0 +1,78 @@
+package Day1.EvaluationTask1;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Product{
+    private String name;
+    private double price;
+    private int quantity;
+    public Product(String name,double price,int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQuantity(){
+        return this.quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+}
+class ShoppingCart{
+    private ArrayList<Product> cart;
+    public ShoppingCart() {
+        this.cart = new ArrayList<>();
+    }
+    public void addProduct(Product product){
+        this.cart.add(product);
+    }
+    public void removeProduct(Product product){
+        if(cart.isEmpty()){
+            System.err.println("Your cart is empty");
+        }
+        else{
+            cart.remove(product);
+        }
+    }
+    public void totalCost(){
+        if(this.cart.isEmpty()){
+            System.out.println("Sorry we can't calculate total for empty cart!");
+            return;
+        }
+        double total =0.0;
+        for(Product product : this.cart){
+            double value =0.0;
+            value = product.getPrice() * product.getQuantity();
+            System.out.println(product.getName()+":  "+product.getQuantity()+"  "+product.getPrice()+" -->"+value);
+            total += value;
+        }
+        System.out.println("--------------------------------------------\n Total Cost :\t"+total);
+    }
+}
+public class OnlineShoppingSystem {
+    public static void main(String[] args) {
+        Product prod1 = new Product("Rice Bag",1200,2);
+        Product prod2 = new Product("Sprite bottle 2l",70,10);
+        Product prod3 = new Product("Note Books",20,5);
+        ShoppingCart cart = new ShoppingCart();
+        cart.addProduct(prod1);
+        cart.addProduct(prod2);
+        cart.addProduct(prod3);
+        cart.removeProduct(prod2);
+        cart.totalCost();
+
+
+    }
+}
