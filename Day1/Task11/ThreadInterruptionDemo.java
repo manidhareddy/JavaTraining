75d57b9b439197db1742ed04370e45f6df6247ad
Manidhar Reddy <manidhar298@gmail.com>
2023-11-07 14:45:18 +0530

    Thread interruption demo

---
+++ Day1/Task11/ThreadInterruptionDemo.java
@@ -0,0 +1,26 @@
+class Application implements Runnable{
+	@Override
+	public void run(){
+		for(int i = 0 ; i < Integer.MAX_VALUE ; i++){
+			if(Thread.interrupted()){
+				System.out.println("This Thread got interrupted by main thread");
+				System.err.println("Try Again");
+				return;
+			}
+			System.out.println("Downloding File compeleted .........." + i);
+		}
+	}
+}
+public class ThreadInterruptionDemo{
+	public static void main(String[] args){
+		try{
+			Thread thread = new Thread(new Application());
+			thread.start();
+			Thread.currentThread().sleep(200);
+			thread.interrupt();
+		}
+		catch(InterruptedException e){
+			System.out.println(e);
+		}
+	}
+}
