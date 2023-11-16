cf8523aa10ce683f3dadf16efed368413176c067
Manidhar Reddy <manidhar298@gmail.com>
2023-11-16 15:04:30 +0530

    using java stream API

--- Day1/Task15/ConvertToUppercase.java
+++ Day1/Task15/ConvertToUppercase.java
@@ -1,2 +1,14 @@
-package Day1.Task15;public class ConvertToUppercase {
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class ConvertToUppercase {
+    public static void main(String[] args) {
+        List<String> stringList = List.of("Chicony" , "Acer" ,  "Lenovo" ,"Macbook" , "Asus" , "Hp");
+		System.out.println(stringList);
+		stringList = stringList.stream().map(word -> !word.startsWith("A") ? word.toUpperCase() : word).collect(Collectors.toList());
+		System.out.println(stringList);
+        
+    }
 }
